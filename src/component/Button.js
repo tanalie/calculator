24824858a747e4255eefa16d900106362f6d46f8
ddaGarrido <36745373+ddaGarrido@users.noreply.github.com>
2020-10-07 12:13:56 -0300

    update color of button

--- src/component/Button.js
+++ src/component/Button.js
@@ -17,7 +17,7 @@ export default class Button extends React.Component {
   render() {
     const className = [
       "component-button",
-      this.props.orange ? "orange" : "",
+      this.props.orange ? "red" : "green",
       this.props.wide ? "wide" : "",
     ];
 
